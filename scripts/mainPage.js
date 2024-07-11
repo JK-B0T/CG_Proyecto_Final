a03889543182e9720d921626d9c7248c9da26dd4
JK-B0T <JK-B0T@users.noreply.github.com>
2024-07-11 13:44:29 +0200

    Fix Bug

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,5 +1,19 @@
 window.addEventListener("DOMContentLoaded", main, false);
+console.log("hola")
 
 function main() {
-    
+    const openItemShopBtn = document.querySelector("#openItemShop");
+    const openUnitShopBtn = document.querySelector("#openUnitShop");
+    const unitArray = Array.from(document.querySelectorAll("unitContainer"));
+
+    openItemShopBtn.addEventListener("click", openSection ,false);
+    openUnitShopBtn.addEventListener("click", openSection ,false);
+    unitArray.forEach(element => {
+        element.addEventListener("click", openSection ,false)
+    });
+
+    function openSection(e) {
+        console.log(e.target);
+        //if(e.target)
+    }
 }
